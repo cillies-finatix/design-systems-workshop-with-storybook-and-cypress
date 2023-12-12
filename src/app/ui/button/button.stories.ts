61a173c28816ad9337551edbf6e5c230b4588cb0
Christian Illies (FINATIX) <c.illies@finatix.de>
2023-12-12 11:49:41 +0100

    add tailwind

--- src/app/ui/button/button.stories.ts
+++ src/app/ui/button/button.stories.ts
@@ -3,12 +3,15 @@ import type { Meta, StoryObj } from '@storybook/angular';
 import { ButtonComponent } from './button.component';
 
 const meta: Meta<ButtonComponent> = {
-  title: 'Components/Button',
+  title: 'UI/Button',
   component: ButtonComponent,
 };
 
 export const Default: StoryObj<ButtonComponent> = {
-  args: {},
+  args: {
+    type: 'primary',
+    label: 'Your Button label'
+  },
 };
 
 export default meta;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
   theme: {
+    colors: {
+      'blue': {
+        '50': '#f0f7ff',
+        '100': '#e0eefe',
+        '200': '#badefd',
+        '300': '#7dc4fc',
+        '400': '#38a6f8',
+        '500': '#0e8be9',
+        '600': '#026cc7',
+        '700': '#0357a3',
+        '800': '#074a85',
+        '900': '#0c3e6e',
+        '950': '#082749',
+      },
+      'gray': {
+        '50': '#f4f6f9',
+        '100': '#eceff3',
+        '200': '#dce1e9',
+        '300': '#c6cedb',
+        '400': '#aeb8cb',
+        '500': '#99a2bb',
+        '600': '#7c84a5',
+        '700': '#6f7593',
+        '800': '#5b6078',
+        '900': '#4d5162',
+        '950': '#2d2f39',
+      },
+      'white': '#FFFFFF',
+      'default': '#041C31',
+    },
+    fontFamily: {
+      sans: ["Inter", "Helvetica", "Arial", "sans-serif"],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
